2282aafef4250b9bcfad83a5d1c321f621bf0a6f
mimamofficial1 <muzaffarimamneema@gmail.com>
2026-04-25 10:32:07 +0530

    Update start.py

--- VideoEncoder/plugins/start.py
+++ VideoEncoder/plugins/start.py
@@ -51,7 +51,7 @@ async def start_message(app, message):
 ⚠️ Personal Use Only
 👉 Send Video to Start 🚀</b>"""
     await message.reply_photo(
-        photo="http://telegraph.controller.bot/files/6630683090/AgACAgUAAxkBAAJ792nsShJWxyjagwxn1wao-VYSoVw-AAIMFGsb2_BpV7ATrrYUmYdcAQADAgADeQADOwQ",
+        photo="https://n.uguu.se/DtZgwBkS.jpg",
         caption=text,
         reply_markup=start_but
     )
